8787f0237c921a8510c7be7fa398f77a3e1ba034
piracy <ohhyearicky@gmail.com>
2025-05-04 15:43:19 -0500

    Update nov.lua

--- nov.lua
+++ nov.lua
@@ -1,7 +1,7 @@
 local placeId = game.PlaceId
 
 if placeId == 112105795812232 then
-    loadstring(game:HttpGet("https://pastebin.com/raw/XypGBWzj"))()
+    loadstring(game:HttpGet("https://pastebin.com/raw/JjHubLgu"))()
 else
     loadstring(game:HttpGet("https://pastebin.com/raw/d7mDq8Pi"))()
 end
